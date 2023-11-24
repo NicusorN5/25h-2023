a1e18bdad5ec0918e0b79c45527698784e6d43f7
Blacktrecks <107044395+Blacktrecks@users.noreply.github.com>
2023-11-24 04:49:25 +0200

    add new buttons

--- 25h/hue-control-interface/src/App.js
+++ 25h/hue-control-interface/src/App.js
@@ -2,14 +2,19 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCarBattery, faLightbulb } from '@fortawesome/free-solid-svg-icons';
-import Device from "./components/Device.js";
+import { faCarBattery } from '@fortawesome/free-solid-svg-icons';
+import './App.css';
 
 const App = () => {
   const [lights, setLights] = useState([]);
   const [selectedLightId, setSelectedLightId] = useState(16);
+  const [isLightOn, setIsLightOn] = useState(false);
+  const [isAllLightsConnected, setIsAllLightsConnected] = useState(false);
+  const [brightness, setBrightness] = useState(50);
+  const [color, setColor] = useState(50);
+  const [saturation, setSaturation] = useState(50);
+  const [temperature, setTemperature] = useState(50);
 
   useEffect(() => {
     fetchData();
@@ -21,45 +26,63 @@ const App = () => {
         'http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights'
       );
       setLights(Object.values(response.data));
+      const selectedLight = response.data[selectedLightId];
+      setIsLightOn(selectedLight?.state?.on || false);
+      setBrightness(selectedLight?.state?.bri || 50);
+      setColor(
+        selectedLight?.state?.hue ? Math.round((selectedLight.state.hue / 65535) * 100) : 50
+      );
+      setSaturation(selectedLight?.state?.sat || 50);
+      setTemperature(
+        selectedLight?.state?.ct ? Math.round((selectedLight.state.ct / 500) * 100) : 50
+      );
     } catch (error) {
       console.error('Error fetching lights:', error);
     }
   };
 
-  const toggleLight = async (isOn) => {
+  const toggleLight = async () => {
     try {
-      await axios.put(
-        `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
-        { on: !isOn }
-      );
-      // Update the state after successfully toggling the light
+      if (!isLightOn) {
+        await axios.put(
+          `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
+          { on: true, xy: [0.313, 0.329], ct: 366 }
+        );
+      } else {
+        await axios.put(
+          `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
+          { on: false }
+        );
+      }
       fetchData();
     } catch (error) {
       console.error('Error toggling light:', error);
     }
   };
 
-  const changeBrightness = async (brightness) => {
+  const changeBrightness = async () => {
     try {
+      const calculatedBrightness = Math.round((brightness / 100) * 254);
       await axios.put(
         `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
-        { bri: brightness }
+        { bri: calculatedBrightness }
       );
-      // Update the state after successfully changing brightness
       fetchData();
+      openLight();
     } catch (error) {
       console.error('Error changing brightness:', error);
     }
   };
 
-  const changeColor = async (color) => {
+  const changeColor = async () => {
     try {
+      const calculatedColor = Math.round((color / 100) * 65535);
       await axios.put(
         `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
-        { hue: color }
+        { hue: calculatedColor }
       );
-      // Update the state after successfully changing color
       fetchData();
+      openLight();
     } catch (error) {
       console.error('Error changing color:', error);
     }
@@ -67,20 +90,20 @@ const App = () => {
 
   const randomColor = async () => {
     try {
-      const color = Math.floor(Math.random() * 65536);
+      const calculatedColor = Math.floor(Math.random() * 65536);
       await axios.put(
         `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
-        { hue: color, sat: 255 } // Ensure maximum saturation for vivid colors
+        { hue: calculatedColor, sat: 255 }
       );
-      // Update the state after successfully changing color
       fetchData();
+      openLight();
     } catch (error) {
       console.error('Error changing color:', error);
     }
   };
 
   const connectAllLights = async () => {
-    const lightsToConnect = [13, 12, 16, 14, 15, 17]; // Add or modify the light IDs as needed
+    const lightsToConnect = [13, 12, 16, 14, 15, 17];
 
     try {
       await Promise.all(
@@ -91,99 +114,222 @@ const App = () => {
           );
         })
       );
-
-      // Update the state after successfully connecting all lights
       fetchData();
+      setIsAllLightsConnected(true);
     } catch (error) {
       console.error('Error connecting lights:', error);
     }
   };
 
-  const resetLights = async () => {
-    try {
-      await axios.put(
-        `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
-        { on: false, bri: 254, hue: 0 }
-      );
-      // Update the state after successfully resetting the light
-      fetchData();
-    } catch (error) {
-      console.error('Error resetting light:', error);
-    }
-  };
+  const disconnectAllLights = async () => {
+    const lightsToDisconnect = [13, 12, 16, 14, 15, 17];
 
-  const turnOffAllLights = async () => {
-    const lightsToTurnOff = [13, 12, 16, 14, 15, 17]; // Add or modify the light IDs as needed
-  
     try {
       await Promise.all(
-        lightsToTurnOff.map(async (lightId) => {
+        lightsToDisconnect.map(async (lightId) => {
           await axios.put(
             `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${lightId}/state`,
             { on: false }
           );
         })
       );
-      // Update the state after successfully turning off all lights
       fetchData();
+      setIsAllLightsConnected(false);
     } catch (error) {
-      console.error('Error turning off lights:', error);
+      console.error('Error disconnecting lights:', error);
+    }
+  };
+
+  const changeTemperature = async () => {
+    try {
+      const calculatedTemperature = Math.round((temperature / 100) * 500);
+      await axios.put(
+        `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
+        { ct: calculatedTemperature }
+      );
+      fetchData();
+      openLight();
+    } catch (error) {
+      console.error('Error changing temperature:', error);
+    }
+  };
+
+  const changeSaturation = async () => {
+    try {
+      await axios.put(
+        `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
+        { sat: saturation }
+      );
+      fetchData();
+      openLight();
+    } catch (error) {
+      console.error('Error changing saturation:', error);
+    }
+  };
+
+  const colorCycle = async () => {
+    try {
+      await axios.put(
+        `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
+        { effect: 'colorloop' }
+      );
+    } catch (error) {
+      console.error('Error starting color cycle:', error);
+    }
+  };
+
+  const emergencyAlert = async (color) => {
+    try {
+      const calculatedColor = color === 'green' ? 25500 : 0;
+      await axios.put(
+        `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
+        { on: true, ct: calculatedColor }
+      );
+    } catch (error) {
+      console.error('Error setting emergency alert:', error);
+    }
+  };
+
+  const handleRangeChange = (slider, value) => {
+    if (slider === 'brightness') {
+      setBrightness(value);
+      changeBrightness();
+    } else if (slider === 'color') {
+      setColor(value);
+      changeColor();
+    } else if (slider === 'saturation') {
+      setSaturation(value);
+      changeSaturation();
+    } else if (slider === 'temperature') {
+      setTemperature(value);
+      changeTemperature();
     }
 
-    
+    openLight();
+  };
+
+  const openLight = async () => {
+    try {
+      await axios.put(
+        `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
+        { on: true }
+      );
+      fetchData();
+    } catch (error) {
+      console.error('Error opening light:', error);
+    }
   };
 
   return (
     <div className="app">
       <div className="navbar">
-        <h1 className="text-start main-title">Welcome! <FontAwesomeIcon icon={faCarBattery} /></h1>
+        <h1 className="text-start main-title">
+          Welcome! <FontAwesomeIcon icon={faCarBattery} />
+        </h1>
       </div>
-      <Device />
       <div className="menu">
         <div className="card">
-          <button onClick={() => toggleLight(lights.find((light) => light.id === selectedLightId)?.state.on || false)} className="button">
-            {lights.find((light) => light.id === selectedLightId)?.state.on ? 'Turn Off' : 'Turn On'}
+          <button onClick={toggleLight} className={`button ${isLightOn ? 'on' : 'off'}`}>
+            {isLightOn ? 'Turn Off' : 'Turn On'}
           </button>
         </div>
         <div className="card">
-          <button onClick={() => turnOffAllLights()} className="button">Turn Off All Lights</button>
+          <button
+            onClick={isAllLightsConnected ? disconnectAllLights : connectAllLights}
+            className="button"
+          >
+            {isAllLightsConnected ? 'Disconnect All Lights' : 'Connect All Lights'}
+          </button>
         </div>
         <div className="card">
-          <div className="brightness-control">
-            <label>Brightness:</label>
-            <input
-              type="range"
-              min="1"
-              max="254"
-              value={lights.find((light) => light.id === selectedLightId)?.state.bri || 0}
-              onChange={(e) => changeBrightness(parseInt(e.target.value))}
-            />
+          <label htmlFor="slider1">Brightness:</label>
+          <div className="progress-bar">
+            <div className="progress-fill" style={{ width: `${brightness}%` }}>
+              <span>{brightness}%</span>
+            </div>
           </div>
+          <input
+            type="range"
+            id="slider1"
+            name="slider1"
+            min="0"
+            max="100"
+            value={brightness}
+            onChange={(e) => handleRangeChange('brightness', e.target.value)}
+          />
         </div>
         <div className="card">
-          <div className="color-control">
-            <label>Color:</label>
-            <input
-              type="range"
-              min="0"
-              max="65535"
-              value={lights.find((light) => light.id === selectedLightId)?.state.hue || 0}
-              onChange={(e) => changeColor(parseInt(e.target.value))}
-            />
+          <label htmlFor="slider2">Color:</label>
+          <div className="progress-bar">
+            <div className="progress-fill" style={{ width: `${color}%` }}>
+              <span>{color}%</span>
+            </div>
           </div>
+          <input
+            type="range"
+            id="slider2"
+            name="slider2"
+            min="0"
+            max="100"
+            value={color}
+            onChange={(e) => handleRangeChange('color', e.target.value)}
+          />
         </div>
         <div className="card">
-          <button onClick={() => randomColor()} className="button">Random Color</button>
+          <label htmlFor="slider3">Saturation:</label>
+          <div className="progress-bar">
+            <div className="progress-fill" style={{ width: `${saturation}%` }}>
+              <span>{saturation}%</span>
+            </div>
+          </div>
+          <input
+            type="range"
+            id="slider3"
+            name="slider3"
+            min="0"
+            max="100"
+            value={saturation}
+            onChange={(e) => handleRangeChange('saturation', e.target.value)}
+          />
         </div>
         <div className="card">
-          <button onClick={() => {/* Add logic for color cycle or animation */}} className="button">Color Cycle</button>
+          <label htmlFor="slider4">Temperature:</label>
+          <div className="progress-bar">
+            <div className="progress-fill" style={{ width: `${temperature}%` }}>
+              <span>{temperature}%</span>
+            </div>
+          </div>
+          <input
+            type="range"
+            id="slider4"
+            name="slider4"
+            min="0"
+            max="100"
+            value={temperature}
+            onChange={(e) => handleRangeChange('temperature', e.target.value)}
+          />
         </div>
         <div className="card">
-          <button onClick={() => resetLights()} className="button">Reset</button>
+          <button onClick={randomColor} className="button">
+            Random Color
+          </button>
         </div>
-      </div>
-      <div className="card">
-        <button onClick={() => connectAllLights()} className="button">Connect All Lights</button>
+        <div className="card">
+          <button onClick={colorCycle} className="button">
+            Color Cycle
+          </button>
+        </div>
+        <div className="card">
+          <button onClick={() => emergencyAlert('green')} className="button">
+            Emergency Alert (Green)
+          </button>
+        </div>
+        <div className="card">
+          <button onClick={() => emergencyAlert('red')} className="button">
+            Emergency Alert (Red)
+          </button>
+        </div>
+        
       </div>
     </div>
   );
--- 25h/hue-control-interface/src/components/Device.js
+++ 25h/hue-control-interface/src/components/Device.js
@@ -1,64 +1,190 @@
-import React, { useState } from 'react';
-import './Device.css';
+// App.js
 
-export default function Device(props) {
-  const [rangePercent1, setRangePercent1] = useState(0);
-  const [rangePercent2, setRangePercent2] = useState(0);
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCarBattery } from '@fortawesome/free-solid-svg-icons';
+import './App.css';
 
-  const handleRangeChange = (sliderId, event) => {
-    const value = event.target.value;
-    if (sliderId === 1) {
-      setRangePercent1(value);
-    } else if (sliderId === 2) {
-      setRangePercent2(value);
+const App = () => {
+  const [lights, setLights] = useState([]);
+  const [selectedLightId, setSelectedLightId] = useState(16);
+  const [isLightOn, setIsLightOn] = useState(false);
+  const [isAllLightsConnected, setIsAllLightsConnected] = useState(false);
+  const [brightnessOrColor, setBrightnessOrColor] = useState('brightness');
+  const [rangePercent, setRangePercent] = useState(50);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        'http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights'
+      );
+      setLights(Object.values(response.data));
+      const selectedLight = response.data[selectedLightId];
+      setIsLightOn(selectedLight?.state?.on || false);
+    } catch (error) {
+      console.error('Error fetching lights:', error);
+    }
+  };
+
+  const toggleLight = async () => {
+    try {
+      await axios.put(
+        `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
+        { on: !isLightOn }
+      );
+      // Update the state after successfully toggling the light
+      fetchData();
+    } catch (error) {
+      console.error('Error toggling light:', error);
+    }
+  };
+
+  const changeBrightnessOrColor = async () => {
+    try {
+      const value = Math.round((rangePercent / 100) * (brightnessOrColor === 'brightness' ? 254 : 65535));
+      await axios.put(
+        `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
+        { [brightnessOrColor === 'brightness' ? 'bri' : 'hue']: value }
+      );
+      // Update the state after successfully changing brightness or color
+      fetchData();
+      // Open the light when changing brightness or color
+      openLight();
+    } catch (error) {
+      console.error('Error changing brightness or color:', error);
+    }
+  };
+
+  const randomColor = async () => {
+    try {
+      const calculatedColor = Math.floor(Math.random() * 65536);
+      await axios.put(
+        `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
+        { hue: calculatedColor, sat: 255 } // Ensure maximum saturation for vivid colors
+      );
+      // Update the state after successfully changing color
+      fetchData();
+      // Open the light when selecting random color
+      openLight();
+    } catch (error) {
+      console.error('Error changing color:', error);
+    }
+  };
+
+  const connectAllLights = async () => {
+    const lightsToConnect = [13, 12, 16, 14, 15, 17]; // Add or modify the light IDs as needed
+
+    try {
+      await Promise.all(
+        lightsToConnect.map(async (lightId) => {
+          await axios.put(
+            `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${lightId}/state`,
+            { on: true }
+          );
+        })
+      );
+
+      // Update the state after successfully connecting all lights
+      fetchData();
+      setIsAllLightsConnected(true);
+    } catch (error) {
+      console.error('Error connecting lights:', error);
+    }
+  };
+
+  const disconnectAllLights = async () => {
+    const lightsToDisconnect = [13, 12, 16, 14, 15, 17]; // Add or modify the light IDs as needed
+
+    try {
+      await Promise.all(
+        lightsToDisconnect.map(async (lightId) => {
+          await axios.put(
+            `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${lightId}/state`,
+            { on: false }
+          );
+        })
+      );
+
+      // Update the state after successfully disconnecting all lights
+      fetchData();
+      setIsAllLightsConnected(false);
+    } catch (error) {
+      console.error('Error disconnecting lights:', error);
+    }
+  };
+
+  const handleRangeChange = (value) => {
+    setRangePercent(value);
+    // Open the light when changing color or brightness
+    openLight();
+  };
+
+  const openLight = async () => {
+    try {
+      await axios.put(
+        `http://192.168.0.10/api/nT3-GptvjYpdzarNevlY993gwFakZTOzwZuvifYp/lights/${selectedLightId}/state`,
+        { on: true }
+      );
+      // Update the state after successfully opening the light
+      fetchData();
+    } catch (error) {
+      console.error('Error opening light:', error);
     }
   };
 
   return (
-    <div className="App card-horizontal">
-      <button type="button" className="btn btn-primary">
-        Play
-      </button>
-
-      <div className="mt-4">
-        <label htmlFor="slider1">Slider 1:</label>
-        <div className="progress-bar">
-          <div
-            className="progress-fill"
-            style={{ width: `${rangePercent1}%` }}
-          >
-            <span>{rangePercent1}%</span>
-          </div>
+    <div className="app">
+      <div className="navbar">
+        <h1 className="text-start main-title">Welcome! <FontAwesomeIcon icon={faCarBattery} /></h1>
+      </div>
+      <div className="menu">
+        <div className="card">
+          <button onClick={toggleLight} className="button">
+            {isLightOn ? 'Turn Off' : 'Turn On'}
+          </button>
         </div>
-        <input
-          type="range"
-          id="slider1"
-          name="slider1"
-          min="0"
-          max="100"
-          value={rangePercent1}
-          onChange={(e) => handleRangeChange(1, e)}
-        />
-      
-        <label htmlFor="slider2">Slider 2:</label>
-        <div className="progress-bar">
-          <div
-            className="progress-fill"
-            style={{ width: `${rangePercent2}%` }}
-          >
-            <span>{rangePercent2}%</span>
-          </div>
+        <div className="card">
+          <button onClick={isAllLightsConnected ? disconnectAllLights : connectAllLights} className="button">
+            {isAllLightsConnected ? 'Disconnect All Lights' : 'Connect All Lights'}
+          </button>
         </div>
-        <input
-          type="range"
-          id="slider2"
-          name="slider2"
-          min="0"
-          max="100"
-          value={rangePercent2}
-          onChange={(e) => handleRangeChange(2, e)}
-        />
+        <div className="card">
+          <label htmlFor="slider">Brightness/Color:</label>
+          <div className="progress-bar">
+            <div
+              className="progress-fill"
+              style={{ width: `${rangePercent}%` }}
+            >
+              <span>{rangePercent}%</span>
+            </div>
+          </div>
+          <input
+            type="range"
+            id="slider"
+            name="slider"
+            min="0"
+            max="100"
+            value={rangePercent}
+            onChange={(e) => handleRangeChange(e.target.value)}
+          />
+        </div>
+        <div className="card">
+          <button onClick={randomColor} className="button">Random Color</button>
+        </div>
+        <div className="card">
+          <button onClick={() => {/* Add logic for color cycle or animation */}} className="button">Color Cycle</button>
+        </div>
+      </div>
+      <div className="footer">
+        {/* Your footer content here */}
       </div>
     </div>
   );
-}
+};
+
+export default App;
